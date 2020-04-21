e7f376308cd893b7d17b5de07839b971e026f37e
Stef Spermalie <stef.vercaemer@spermalie.be>
2020-04-21 15:47:41 +0200

    Add standard code.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,37 @@
 "use strict";
+var btnSlaOp;
+var divFeedBack, divMotor, divOpties;
+var pre;
+var slcAutos;
+var txtMerk, txtType;
 
+window.addEventListener('load', Initieer);
 
-window.addEventListener('load', Initieer);
+function Initieer() {
+	KoppelElementen();
+	KoppelEvents();
+	VulStandaardwaarden();
+};
 
-function Initieer() {
+const KoppelElementen = () => {
+btnSlaOp = document.getElementById("btnSlaOp");
+divFeedBack = document.getElementById("divFeedBack");
+divMotor = document.getElementById("divMotor");
+divOpties = document.getElementById("divOpties");
+pre = document.getElementById("pre");
+slcAutos = document.getElementById("slcAutos");
+txtMerk = document.getElementById("txtMerk");
+txtType = document.getElementById("txtType");
+}
 
+const KoppelEvents = () => {
+	btnSlaOp.addEventListener('click', () => {
+                        
+	});
+}
+
+const VulStandaardwaarden = () => {
+	slcAutos.selectedIndex = "0";
+	txtMerk.value = "";
+	txtType.value = "";
 }
